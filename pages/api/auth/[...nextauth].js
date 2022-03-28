fa00b557e6cee7e7a3d3a40f422fc7765d36d5ba
menglycheng <cheng.mengly19@kti.edu.kh>
2022-03-28 23:56:07 +0700

    FIXED: Fix next-auth

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -2,7 +2,7 @@ import NextAuth from 'next-auth'
 import GoogleProvider from 'next-auth/providers/google'
 export default NextAuth({
   // Configure one or more authentication providers
-  Site: process.env.NEXTAUTH_URL,
+  Site: `${process.env.NEXTAUTH_URL}`,
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID,
